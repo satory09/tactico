6bcdf855a633aa5f3653ff7e2a8849ecdfaaa96d
satory09 <v.etm08@icloud.com>
2026-09-20 22:05:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'tactico-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/53/53283.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
